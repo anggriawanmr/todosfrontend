95a0c52dfdb0a46e03f5a2314113f39ce27f2453
anggriawanmr <anggriawanmr@gmail.com>
2021-08-16 09:30:32 +0700

    Adding Form

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TodoItem from "./TodoItem";
+import TodoForm from "./TodoForm";
 const APIURL = "http://localhost:3000/api/todos";
 
 class TodoList extends Component {
@@ -34,11 +35,39 @@ class TodoList extends Component {
       .then((todos) => this.setState({ todos }));
   }
 
+  addTodo(val) {
+    fetch(APIURL, {
+      method: "post",
+      headers: new Headers({
+        "Conten-Type": "application/json",
+      }),
+      body: JSON.stringify({ name: val }),
+    })
+      .then((resp) => {
+        if (!resp.ok) {
+          if (resp.status >= 400 && resp.status < 500) {
+            return resp.json().then((data) => {
+              let err = { errorMessage: data.message };
+              throw err;
+            });
+          } else {
+            let err = { errorMessage: "Please try again later, server is not responding" };
+            throw err;
+          }
+        }
+        return resp.json();
+      })
+      .then((newTodo) => {
+        this.setState({ todos: [...this.state.todos, newTodo] });
+      });
+  }
+
   render() {
     const todos = this.state.todos.map((t) => <TodoItem key={t._id} {...t} />);
     return (
       <div>
         <h1>Todo List</h1>
+        <TodoForm addTodo={this.addTodo} />
         <ul>{todos}</ul>
       </div>
     );
